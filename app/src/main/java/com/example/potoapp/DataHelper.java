5f35657945e255243ad54d12a69d35215f1781d5
ABahamondes <andresbcarvajal4982@gmail.com>
2023-10-04 12:40:52 -0300

    feat DataHelper

---
+++ app/src/main/java/com/example/potoapp/DataHelper.java
@@ -0,0 +1,26 @@
+package com.example.potoapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataHelper extends SQLiteOpenHelper {
+
+
+    public DataHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE servicio (id INTEGER PRIMARY KEY, address TEXT, reference TEXT, obac TEXT, type TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS servicio");
+        db.execSQL("CREATE TABLE servicio (id INTEGER PRIMARY KEY, address TEXT, reference TEXT, obac TEXT, type TEXT)");
+    }
+}
